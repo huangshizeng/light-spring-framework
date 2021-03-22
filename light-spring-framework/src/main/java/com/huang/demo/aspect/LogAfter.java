2a31924ef43eac7d07afe7c5037e03f47f534ab4
huangshizeng <hsz101@qq.com>
2021-03-22 14:37:57 +0800

    支持Before和AfterReturning的aop

--- light-spring-framework/src/main/java/com/huang/demo/aspect/LogAfter.java
+++ light-spring-framework/src/main/java/com/huang/demo/aspect/LogAfter.java
@@ -1,10 +1,22 @@
 package com.huang.demo.aspect;
 
+import com.huang.springframework.aop.advice.MethodAfterReturningAdvice;
+import com.huang.springframework.aop.annotation.Aspect;
+
+import java.lang.reflect.Method;
+
 /**
  * @author: hsz
  * @date: 2021/3/22 14:17
  * @description:
  */
 
-public class LogAfter {
+@Aspect("execution(* com.huang.demo.controller.TestController.*(..))")
+public class LogAfter implements MethodAfterReturningAdvice {
+
+    @Override
+    public Object afterReturning(Object target, Method method, Object[] args, Object returnValue) throws Throwable {
+        System.out.println("【LogBefore】方法调用后:");
+        return null;
+    }
 }
